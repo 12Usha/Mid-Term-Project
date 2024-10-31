96320e082651db767be1efd0d795b634df00afe2
Deepak PC <Neupanechhabi1@gmail.com>
2024-10-30 22:06:09 -0500

    rest api commits

---
+++ src/main/java/org/example/WelcomeController.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome Page";
+    }
+}
+
